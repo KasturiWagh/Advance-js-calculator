903f993368666c212ad587a5fc4f5b441239f1f2
kasturi wagh <kasturiwagh9@gmail.com>
2021-10-28 22:58:06 +0530

    add more buttons

--- index.js
+++ index.js
@@ -39,6 +39,61 @@ for (item of buttons) {
             screen.value += buttonText;
         }
 
+        else if(buttonText == 'π'){
+            buttonText = Math.PI;
+            screen.value += buttonText;
+        }
+
+        else if(buttonText == 'ln'){
+            buttonText = screen.value;
+            screen.value = Math.log(buttonText);
+        }
+
+        else if(buttonText == 'log10'){
+            buttonText = screen.value;
+            screen.value = Math.log10(buttonText);
+        }
+
+        else if(buttonText == '√'){
+            buttonText = screen.value;
+            screen.value = Math.sqrt(buttonText);
+        }
+
+        else if(buttonText == 'x^2'){
+            buttonText = screen.value;
+            screen.value = Math.pow(buttonText,2);
+        }
+
+        else if(buttonText == 'sin'){
+            buttonText =  screen.value;
+            screen.value = Math.sin(buttonText);
+        }
+
+        else if(buttonText == 'cos'){
+            buttonText = screen.value;
+            screen.value = Math.cos(buttonText);
+        }
+
+        else if(buttonText == 'tan'){
+            buttonText = screen.value;
+            screen.value = Math.tan(buttonText);
+
+        }
+
+        else if(buttonText == '!'){
+            buttonText = screen.value;
+            let ans = 1;
+            if(buttonText == 0 || buttonText == 1){
+                screen.value = 1;
+            }
+            else{
+                for (let i = buttonText; i >= 1; i--) {
+                   ans =  ans *i;      
+                }
+                screen.value = ans;
+            }
+        }
+
         else{
             screen.value += buttonText;
         }
